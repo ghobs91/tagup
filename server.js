42c3c1cbba4d1c0a44cec9bb58fa782e7820c636
Andrew <ghobrial.andrew@gmail.com>
2019-04-08 01:09:44 -0400

    get request for all restaurants in list

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ server.use(express.json());
 const knex = require('knex');
 const knexConfig = require('./knexfile');
 const db = knex(knexConfig.development);
+///////
+
+// List all the records
+server.get('/api/list', (req, res) => {
+  db("restaurants").then(restaurants => {
+    res.status(200).json(restaurants);
+    console.log('GET request complete')
+  }).catch(error => {
+      res.status(500).json({ error: "Error retrieving restaurants", info: error })
+  });
+});
